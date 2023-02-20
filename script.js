709efe52cbae41ee5c221876b19d609bfbee026c
Kai Eugene <kai.yougene@gmail.com>
2023-02-20 11:59:28 -0800

    random color and reset functionality

--- script.js
+++ script.js
@@ -29,12 +29,23 @@ function changeSize (input) {
 }
 
 function colorSquare (){
+
+    if (color === 'random'){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else {
     this.style.backgroundColor = color;
+    }
 }
 
 function changeColor (choice) {
     color = choice;
 }
 
+function resetBoard (){
+    let board = document.querySelector('#container');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
 
     
